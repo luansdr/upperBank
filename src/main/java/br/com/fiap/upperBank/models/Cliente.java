20fe0fe4db32af9cb0f678865b43e8e0cfe1fda6
luan.reis <luan.reis@portoseguro.com.br>
2023-05-15 18:06:01 -0300

    bug-fix do projeto

--- src/main/java/br/com/fiap/upperBank/models/Cliente.java
+++ src/main/java/br/com/fiap/upperBank/models/Cliente.java
@@ -1,14 +1,12 @@
 package br.com.fiap.upperBank.models;
 
 import java.util.Calendar;
-import java.util.List;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.ManyToMany;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
